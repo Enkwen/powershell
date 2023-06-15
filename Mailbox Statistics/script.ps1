9b2b553d40a41d8eddd24be84552379c0fa4e573
Enkwen <pavel@enkwen.com>
2023-06-15 16:40:13 +0300

    UTF

--- Mailbox Statistics/script.ps1
+++ Mailbox Statistics/script.ps1
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 Get-ADUser -Filter * -Properties DisplayName,Company,EmailAddress |
 ForEach-Object {
     $mailboxSize = (Get-MailboxStatistics $_.SamAccountName).TotalItemSize.Value.ToMB()
