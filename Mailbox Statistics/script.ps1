4220399c67da837bc8736bee40fb5e77c7dfe3b7
Enkwen <pavel@enkwen.com>
2023-06-15 16:37:35 +0300

    Files added

---
+++ Mailbox Statistics/script.ps1
@@ -0,0 +1,8 @@
+Get-ADUser -Filter * -Properties DisplayName,Company,EmailAddress |
+ForEach-Object {
+    $mailboxSize = (Get-MailboxStatistics $_.SamAccountName).TotalItemSize.Value.ToMB()
+    Write-Host "ФИО: $($_.DisplayName)"
+    Write-Host "Организация: $($_.Company)"
+    Write-Host "Почта: $($_.EmailAddress)"
+    Write-Host "Размер ящика: $mailboxSize MB"
+}
