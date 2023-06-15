b6815a5adb0b5f654caf9ebfbe62994a84320463
Enkwen <pavel@enkwen.com>
2023-06-15 17:16:46 +0300

    save to csv

--- Mailbox Statistics/script.ps1
+++ Mailbox Statistics/script.ps1
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-Get-ADUser -Filter {EmailAddress -like '*'} -Properties DisplayName,Company,EmailAddress |
+Get-ADUser -Filter { EmailAddress -like '*' } -Properties DisplayName, Company, EmailAddress |
 ForEach-Object {
     $mailboxSize = (Get-MailboxStatistics $_.SamAccountName).TotalItemSize.Value.ToMB()
-    Write-Host "ФИО: $($_.DisplayName)"
-    Write-Host "Организация: $($_.Company)"
-    Write-Host "Почта: $($_.EmailAddress)"
-    Write-Host "Размер ящика: $mailboxSize MB"
-}
+    $props = @{
+        Name                = $_.DisplayName
+        Company             = $_.Company
+        Email               = $_.EmailAddress
+        "Mailbox Size (MB)" = $mailboxSize
+    }
+    New-Object PSObject -Property $props
+} | Export-Csv -Path C:\Temp\mailboxes.csv -NoTypeInformation -Encoding UTF8
