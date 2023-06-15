00c7873b3906335559b26e5620452d9e5710ebaf
Enkwen <pavel@enkwen.com>
2023-06-15 13:59:36 +0300

    Files added

---
+++ Set OU adminDescription/script.ps1
@@ -0,0 +1,11 @@
+$ouList = Get-ADOrganizationalUnit -Filter * 
+foreach ($ou in $ouList) {
+    $objectGUID = $ou.ObjectGUID
+    Set-ADOrganizationalUnit -Identity $ou.DistinguishedName -add @{adminDescription = $objectGUID}
+}
+
+
+
+
+
+
